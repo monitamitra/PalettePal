037e3e9048ab56d044997ea47251f812b12aead3
Monita <mitramonita@gmail.com>
2025-05-30 21:37:06 -0500

    attempting to deploy spring boot backend to google cloud

--- backend/my-backend/src/main/java/com/palettepal/my_backend/security/SecurityConfig.java
+++ backend/my-backend/src/main/java/com/palettepal/my_backend/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("https://*.vercel.app"));
+        config.setAllowedOrigins(List.of("https://palette-pal.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
